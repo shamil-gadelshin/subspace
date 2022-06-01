2ab7d488e75f63813b96207c3903667067a451ac
Shamil Gadelshin <shamilgadelshin@gmail.com>
2022-06-01 19:31:07 +0300

    core-primitives: Add serde derivations to PieceIndexhash.

--- crates/subspace-core-primitives/src/lib.rs
+++ crates/subspace-core-primitives/src/lib.rs
@@ -461,6 +461,8 @@ pub type PieceIndex = u64;
 
 /// Hash of `PieceIndex`
 #[derive(Clone, Copy, Debug, PartialEq, Eq, PartialOrd, Ord)]
+#[cfg_attr(feature = "std", derive(Serialize, Deserialize))]
+#[cfg_attr(feature = "std", serde(rename_all = "camelCase"))]
 pub struct PieceIndexHash(pub Sha256Hash);
 
 impl From<PieceIndex> for PieceIndexHash {
